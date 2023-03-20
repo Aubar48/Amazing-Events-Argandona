59aab8d2219e07512e6dbd3ddb52e41e724341e5
“Nahuel <nnahuell.gb@hotmail.com>
2023-03-20 09:37:03 -0300

    Tablas terminadas

--- js/stats.js
+++ js/stats.js
@@ -1,95 +1,134 @@
- 
- // creamos un arreglo de objetos vacio.
- let sortedEventsPast = []
- //vinculamos con el documento html la seccion1 de la tabla a ocupar.
- let tabla1 = document.getElementById('seccion1Table')
- // funcion async, aca traemos ala api mindhub y llamamos alas demas funciones que esperen (await)
-async function getBestEvents() {
-    const response = await fetch('https://mindhub-xj03.onrender.com/api/amazing')
-    const data = await response.json()
-    const ev = await data.events
-    const currentDate = await data.currentDate
-    //filtrar los eventos por eventos pasado del array
-    const pastEvents = await ev.filter(e => e.date < currentDate)
-    //filtrar los eventos por eventos futuros del array
-    const upEvents = await ev.filter(e => e.date >= currentDate)
-    //-------------------table 1
-    // Ordenar los eventos por su capacidad de mayor a menor y traer los primeros 3
-    sortedEventsPast = await (pastEvents.sort((a, b) => b.capacity - a.capacity)).slice(0, 3)
-    // Ordenar los eventos por porcentaje de menor a mayor y trae los primeros 3
-    let pastEventGrowing = await nameX(pastEvents).sort((a, b) => a.percentage - b.percentage).slice(0, 3)
-    // Ordenar los eventos por porcentaje de ayor a menor y trae los primeros 3 (se aplico reverse ala linea anterior)
-    let pastEventDecreasing = await nameX(pastEvents).sort((a, b) => a.percentage - b.percentage).reverse().slice(0, 3)
-    //se pasan los tres arrays para la funcion de crear tabla1 que es la seccion 1 de la tabla.
-    tabla1.innerHTML = await seccion1Tabla(pastEventDecreasing, pastEventGrowing, sortedEventsPast)
-    //----Fin seccion1 de la tabla---
-    
-    // tabla2.innerHTML = await seccion1Tabla(up)
+let eventStatics = document.getElementById("eventStatics");
+let upcomingStatics = document.getElementById("upcomingStatics");
+let pastsStatics = document.getElementById("pastsStatics");
 
-    let upCategoriesTable1 = await upEventsTable(upEvents).sort((a, b) => a.percentage - b.percentage).reverse().slice(0, 3)
+const urlApi = "https://mindhub-xj03.onrender.com/api/amazing";
 
-  }
-  //llamamos ala funcion
-  getBestEvents()
+let allEvents;
 
-  // aca creamos una funcion para pushear elementos en un nuevo array y tener percentage
-  function nameX(array){
-    let nameZ = []
-    for (elemento of array) {
-      nameZ.push({
-        id:elemento._id,
-        name:elemento.name,
-        capacity:elemento.capacity,
-        assistance:elemento.assistance,
-        category:elemento.category,
-        percentage:((elemento.assistance / elemento.capacity)* 100)
-      })
+let currentDate;
+
+async function traerDatos() {
+
+    try {
+        const datos = await fetch(urlApi)
+            .then(response => response.json())
+            .then(data => data);
+
+        allEvents = datos.events;
+        currentDate = datos.currentDate;
+        statsInitializer();
+
+    } catch (error) {
+        console.log(error);
     }
-    return nameZ
-  }
-  // en esta funcion creamos el tbody de la tabla de la seccion 1
-  function seccion1Tabla(array1, array2, array3) { 
-    let tabla1 = '';
-    for (let i = 0; i < 3; i++) {
-        tabla1 += `
-    <tr>
-    <td> ${array1[i].name} ${array1[i].percentage} % </td>
-    <td>${array2[i].name} ${array2[i].percentage} %  - </td>
-    <td>${array3[i].name}- ${array3[i].capacity} personas </td>
-    </tr>
-`;
+}
+traerDatos();
+function statsInitializer() {
+
+
+    let sortEventByPercentageOfAttendance = allEvents.filter(e => e.assistance != undefined).sort((a, b) =>  (b.assistance / b.capacity) - (a.assistance / a.capacity));
+    let sortEventByPercentageOfEstimate = allEvents.filter(e => e.estimate != undefined).sort((a, b) => (a.estimate / a.capacity) - (b.estimate / b.capacity));
+    let sortEventByCapacity = allEvents.filter(e => e.assistance != undefined).sort((a, b) =>  b.capacity - a.capacity);
+
+    let pastArray = [];
+    sortEventByPercentageOfAttendance.map(ev => {
+        if (!pastArray.some((item) => ev.category == item.category)) {
+            pastArray.push({
+                category: ev.category,
+                revenues: ev.price * ev.assistance,
+                capacity: ev.capacity,
+                assistance: ev.assistance
+            });
+        } else if (pastArray.some((item) => ev.category == item.category)) {
+            pastArray.map(e => {
+                if (e.category == ev.category) {
+                    e.capacity += ev.capacity;
+                    e.revenues += ev.price * ev.assistance;
+                    e.assistance += ev.assistance;
+                }
+            })
+        }
+    });
+
+    let upcomingArray=[];
+    sortEventByPercentageOfEstimate.map(ev => {
+        if (!upcomingArray.some((item) => ev.category == item.category)) {
+            upcomingArray.push({
+                category: ev.category,
+                revenues: ev.price * ev.estimate,
+                capacity: ev.capacity,
+                estimate: ev.estimate
+            });
+        } else if (upcomingArray.some((item) => ev.category == item.category)) {
+            upcomingArray.map(e => {
+                if (e.category == ev.category) {
+                    e.capacity += ev.capacity;
+                    e.revenues += ev.price * ev.estimate;
+                    e.estimate += ev.estimate;
+                }
+            })
+        }
+    });
+
+    eventStatics.innerHTML = eventStaticsShow(sortEventByPercentageOfAttendance,sortEventByCapacity);
+    pastsStatics.innerHTML = pastStaticsShow(pastArray);
+    upcomingStatics.innerHTML = updateStaticsShow(upcomingArray);
+
+    function eventStaticsShow(eventsAttendance, eventsCapacity) {
+        let rows = '';
+        
+        rows = `<tr>
+                    <td class="bg-secondary">Events with the highest percentage of attendance</td>
+                    <td class="bg-secondary">Events with the lowest percentage of attendance</td>
+                    <td class="bg-secondary">Event with larger capacity</td>
+                </tr>
+                `;
+        for (let i = 0; i < 3; i++) {
+            rows +=`<tr>
+                    <td>${eventsAttendance[i].name} : ${((eventsAttendance[i].assistance/eventsAttendance[i].capacity)*100).toFixed(2)} %</td >
+                    <td>${eventsAttendance[eventsAttendance.length-i-1].name} : ${((eventsAttendance[eventsAttendance.length-i-1].assistance/eventsAttendance[eventsAttendance.length-i-1].capacity)*100).toFixed(2)} %</td >
+                    <td>${eventsCapacity[i].name} : ${eventsCapacity[i].capacity}</td >
+                </tr>`;
+        }   
+
+        return rows;
     }
-    return tabla1;
+
+    function updateStaticsShow(array){
+        let rows = `<tr>
+                        <td class="bg-secondary">Categories</td>
+                        <td class="bg-secondary">Revenues</td>
+                        <td class="bg-secondary">Percentage of estimate</td>
+                    </tr>`;
+
+        for (const item of array) {
+            rows += `<tr>
+                        <td>${item.category}</td>
+                        <td>$${item.revenues}</td>
+                        <td>${((item.estimate)/(item.capacity)*100).toFixed(2)} %</td>
+                    </tr>`
+        }
+        return rows;
+    }
+
+    function pastStaticsShow(array){
+        let rows = `<tr>
+                        <td class="bg-secondary">Categories</td>
+                        <td class="bg-secondary">Revenues</td>
+                        <td class="bg-secondary">Percentage of attendance</td>
+                    </tr>`;
+
+        for (const item of array) {
+            rows += `<tr>
+                        <td>${item.category}</td>
+                        <td>$${item.revenues}</td>
+                        <td>${((item.assistance)/(item.capacity)*100).toFixed(2)} %</td>
+                    </tr>`
+        }
+        return rows;
+    }
 }
 
-// aca creamos una funcion para pushear elementos en un nuevo array y tener revenues
-function upEventsTable(array){
-  let tabla2 = []
-  for (elemento of array) {
-    tabla2.push({
-      id:elemento._id,
-      name:elemento.name,
-      capacity:elemento.capacity,
-      estimate:elemento.estimate,
-      category:elemento.category,
-      revenues:(elemento.price * elemento.assistance),
-      percentage:((elemento.estimate / elemento.capacity)* 100)
-    })
-  }
-  return tabla2
-}
 
-// en esta funcion creamos el tbody de la tabla de la seccion 2
-function seccion1Tabla2(array1, array1, array1) { 
-  let tabla2 = '';
-  for (let i = 0; i < 3; i++) {
-      tabla2 += `
-  <tr>
-  <td> ${array1[i].category}</td>
-  <td>${array2[i].revenues}</td>
-  <td>${array3[i].percentage} % </td>
-  </tr>
-`;
-  }
-  return tabla2;
-}
+
